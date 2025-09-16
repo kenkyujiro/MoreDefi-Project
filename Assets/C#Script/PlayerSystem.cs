0133373ea22b90617303403c2775d3e66aa15df2
yukihotta <yuuki2628@outlook.jp>
2025-09-16 16:29:29 +0900

    Commit Subject

--- Assets/C#Script/PlayerSystem.cs
+++ Assets/C#Script/PlayerSystem.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class PlayerSystem : MonoBehaviour
 {
+    public float moveSpeed = 5f;  // 横移動速度
     public float jumpForce = 5f;  // ジャンプ力
+
     public Transform groundCheck; // 地面チェック用のTransform
     public LayerMask groundLayer; // 地面レイヤーの設定
     private Rigidbody2D rb;       // Rigidbody2D
+
     private bool isGrounded;      // 地面にいるかどうか
+    private float moveInput;      //横移動用の値
 
     void Start()
     {
@@ -15,6 +19,9 @@ public class PlayerSystem : MonoBehaviour
 
     void Update()
     {
+        //入力を取得(ADに合わせて、-1か1を取得)
+        moveInput = Input.GetAxisRaw("Horizontal");
+
         // 地面にいるかチェック(groundCheck.positionを中心に円形にチェック)
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.8f, groundLayer);
 
@@ -23,6 +30,12 @@ public class PlayerSystem : MonoBehaviour
         {
             Jump();
         }
+    }
+
+    void FixedUpdate()
+    {
+        // 横移動 (物理挙動なので FixedUpdate 内で処理)
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
 
     void Jump()
